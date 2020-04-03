75e2d5363de981851a1ff25c61eb3532e9d6fad7
Christoph Schwizer <christoph@schwizer.dev>
2020-04-03 12:38:53 +0200

    Load venv in run script

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# change into script directory
+cd "$(dirname "$0")"
+
+# load virtualenv
+source ./env/bin/activate
+
+# start flask server
 gunicorn flasksite:app \
     --bind 0.0.0.0:8000 \
     --timeout 120 \
